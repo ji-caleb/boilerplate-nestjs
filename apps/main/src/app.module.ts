1f50ec5b6e375fb455dd8367ed9fb12c9c98a8af
Donghyun-Ji <jicaleb88@gmail.com>
2025-12-30 23:37:32 +0900

    feat: graphql 설정 forRootAsync 변경

--- apps/main/src/app.module.ts
+++ apps/main/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -7,8 +7,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { MongooseConfigService } from '@app/common/configs/mongoose.config';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import { LogLevel } from '@app/common/logger/winston.logger';
+import { GqlConfigService } from '@app/common/configs/graphql.config';
 
 @Module({
   imports: [
@@ -21,36 +20,11 @@ import { LogLevel } from '@app/common/logger/winston.logger';
       imports: [ConfigModule],
       useClass: MongooseConfigService,
     }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
-      playground: false,
-      introspection: true,
-      plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
-      formatError: (error) => {
-        if (!error.extensions?.logged) {
-          const logger = new Logger('GraphQLError');
-          logger.error({
-            level: LogLevel.ERROR,
-            message: error.message,
-            code: error.extensions?.code,
-            stack: error.extensions?.stacktrace,
-          });
-        }
-
-        return {
-          message: error.message,
-          path: error.path,
-          extensions: {
-            code: error.extensions?.code,
-            statusCode: error.extensions?.statusCode ?? 400,
-            message: error.message,
-            ...(process.env.NODE_ENV === 'development' && {
-              stack: error.extensions?.stack ?? error.extensions?.stacktrace,
-            }),
-          },
-        };
-      },
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useClass: GqlConfigService,
     }),
   ],
   controllers: [AppController],
